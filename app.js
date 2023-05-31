a6a3e03eb1fe60e9cfefc17c65ec2011ecaeffac
Priscilla Lau <pyu@wellesley.edu>
2023-05-31 13:30:07 -0400

    added js branch

---
+++ app.js
@@ -0,0 +1,14 @@
+// C-style
+
+//1. define the problem
+//1a. when a user clicks one of my divs
+
+const myBtn = "stuff";
+const myName = "Priscilla Lau";
+const myNumber = "34";
+
+// variable called "myBtn" that has "stuff"
+
+console.log(myBtn + myNumber);
+
+//1b. I see a pop-up
