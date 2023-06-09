5de4eeb5ef747b8decaeaf400b0c9013bcd7bb26
Priscilla Lau <pyu@wellesley.edu>
2023-06-09 13:46:26 -0400

    update from 6/9 class

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ const promoBtn = document.getElementById("promo-btn");
 const coldBtn = document.getElementById("cold-btn");
 const prodBtn = document.getElementById("launch-btn");
 const abandBtn = document.getElementById("abandoned-btn");
-const taskPrompt = document.getElementById("task-prompt");
+const taskPromptTextEl = document.getElementById("task-prompt");
 const taskPromptResultEl = document.getElementById("task-prompt-result");
 const userInputResult = document.getElementById("user-input-result");
 const startOverBtn = document.getElementById("start-over-btn");
-const suggestDropdown = document.getElementById("suggest-dropdown");
+const taskType = document.getElementById("suggest-dropdown");
 
 //when enter is keyed, the text will stay and AI Generated results will appear
-taskPrompt.addEventListener("keydown", (event) => {
+taskPromptTextEl.addEventListener("keydown", (event) => {
   if (event.key === "Enter") {
     taskPromptResultEl.innerHTML = `
         <section class="p-6 bg-slate-200">
@@ -84,34 +84,61 @@ taskPrompt.addEventListener("keydown", (event) => {
         </section>
     `;
     //this creates a variable that holds the value written in the textbox
-    const userInputText = taskPrompt.value;
+    const userInput = taskPromptTextEl.value;
 
     //this prints the variable text into the console log
-    console.log(userInputText);
+    console.log(userInput);
 
-    //this displays the variable text (what is typed into the textbox by the user) onto the screen
-    userInputResult.innerHTML = `
-    <section class="p-8 bg-slate-050">
-      <p class="m-2">
-        <div id="user-input-text"> ${userInputText} </div>
-      </p>
-    </section>
-    `;
-    taskPrompt.value = "";
+    taskPromptTextEl.value = "";
+
+    const generatedPlan = generateTask(userInput); //Data Structure
+
+    showGeneratedPlan(generatedPlan)
   }
 });
 
-//upon clicking the "start over" button, the page will reload
-startOverBtn.addEventListener("click", () => {
-  taskPromptResultEl.innerHTML= ""; // resets the gray AI generated box
-  userInputResult.innerHTML = ""; // resets the white text box
-  suggestDropdown.value = ""; // resets the dropbox
-});
 
-//javascript for dropdown section of the page
+function generateTask (prompt = "generate a day plan based on the __ technique") {
+  
+  //use OpenAI to generate a plan based on a prod. method
+  const generateResponseFromAPI = getGPTResponseData(prompt); //Array
+
+  ///return the data to the caller
+  return generateResponseFromAPI;
+}
+
+//mock a function
+function showGeneratedPlan(generatedPlanInput) {
+  console.log(generatedPlanInput);
+
+
+  // taskPromptResultEl.innerHTML = ""
+}
+
+function showGeneratedPlan(generatedPlanInput) {
+  console.log(generatedPlanInput);
+  // console.log(generatedPlanInput.length === 0);
+  // for(let index; generatedPlanInput.length < index; index++){  }
+  generatedPlanInput.forEach((item) => displayContent(item));
+}
+
+//creates function with two parameters (item to display, location where innerHTML prints)
+function displayContent(aDisplayItem, showResultDOM = taskPromptResultEl) {
+  //will print content of aDisplayItem
+  showResultDOM.innerHTML = `${aDisplayItem}`;
+  console.log(aDisplayItem);
+}
+
+function getGPTResponseData(prompt) {
+  return [
+    "Certainly! The __ technique is a time management method that uses a timer..."
+  ];
+}
+
+//TASK SWITCHER SECTION
 
 //creating event listener so that upon change, the results will update
-suggestDropdown.addEventListener("change", (event) => {
+taskType.addEventListener("change", (event) => {
   updateResult(event.target.value);
 });
 
@@ -130,7 +157,7 @@ const updateResult = (dropdownType) => {
     </section>
   `;
   //OUTCOME 3: white area - inputted text
-    const userInputText = suggestDropdown.value;
+    const userInputText = taskType.value;
     userInputResult.innerHTML = `
     <section class="p-8 bg-slate-050">
       <p class="m-2">
@@ -155,4 +182,14 @@ prodBtn.addEventListener("click", () => {
 
 abandBtn.addEventListener("click", () => {
   updateResult("abandoned cart email sequence");
+});
+
+
+//RESET / START OVER BUTTON
+
+//upon clicking the "start over" button, the page will reload
+startOverBtn.addEventListener("click", () => {
+  taskPromptResultEl.innerHTML= ""; // resets the gray AI generated box
+  userInputResult.innerHTML = ""; // resets the white text box
+  taskType.value = ""; // resets the dropbox
 });
---
+++ readme.md
@@ -0,0 +1,18 @@
+## Overview
+
+This app allows users to generate a day/task plan based on AI using established productivity techinques.
+
+## User Journey
+
+```mermaid
+graph TD;
+A[User] --> B[types in what they want to achieve]
+B --> C[get a plan based on that]
+```
+
+## Application Architecture
+Designing the algorithm for our code, problem
+
+### Use-case 1:
+```mermaid
+A[accept a prompt] --> B[Usethe prompt]
